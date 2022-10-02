6b27fa558c979e9b186d843ace8aa4997f0b781d
Chew <chew@chew.pw>
2022-10-02 00:33:25 -0500

    Initial progress on migration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde_json::json;
+
+async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "rory": {
+            "total": 100,
+            "id": 1,
+            "url": "https://i.rory.cat/OShcAhsR.webp"
+        }
+    });
+
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))}
+    )
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
